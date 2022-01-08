1227ad5ac24e134b47722769b9b2188f1f1ed872
yoonseol <yoonseol1983@gmail.com>
2022-01-09 00:52:34 +0900

    Servlet Filter Login check

--- src/main/java/spring/itemproject/web/WebConfig.java
+++ src/main/java/spring/itemproject/web/WebConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.itemproject.web.filter.LogFilter;
+import spring.itemproject.web.filter.LoginCheckFilter;
 
 import javax.servlet.Filter;
 
@@ -14,11 +15,21 @@ public class WebConfig {
     // 실무에서 HTTP 요청시 같은 요청의 로그에 모두 같은 식별자를 자동으로 남기는 방법 : logback mdc
     @Bean
     public FilterRegistrationBean logFilter() {
-        FilterRegistrationBean<Filter> filterFilterRegistrationBean = new FilterRegistrationBean<>();
-        filterFilterRegistrationBean.setFilter(new LogFilter());
-        filterFilterRegistrationBean.setOrder(1);
-        filterFilterRegistrationBean.addUrlPatterns("/*");
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LogFilter());
+        filterRegistrationBean.setOrder(1);
+        filterRegistrationBean.addUrlPatterns("/*");
 
-        return filterFilterRegistrationBean;
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean loginCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter());
+        filterRegistrationBean.setOrder(2);
+        filterRegistrationBean.addUrlPatterns("/*");
+
+        return filterRegistrationBean;
     }
 }
