7ea7c4621407cd5ca8c46cbaf551a2cf5de410fa
yoonseol <yoonseol1983@gmail.com>
2022-01-09 19:09:08 +0900

    Spring Interceptor Login Check

--- src/main/java/spring/itemproject/web/WebConfig.java
+++ src/main/java/spring/itemproject/web/WebConfig.java
@@ -3,17 +3,35 @@ package spring.itemproject.web;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import spring.itemproject.web.filter.LogFilter;
 import spring.itemproject.web.filter.LoginCheckFilter;
+import spring.itemproject.web.interceptor.LogInterceptor;
+import spring.itemproject.web.interceptor.LoginCheckInterceptor;
 
 import javax.servlet.Filter;
 
 @Configuration
-public class WebConfig {
-    
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/css/**", "/*.ico", "/error");
+
+        registry.addInterceptor(new LoginCheckInterceptor())
+                .order(2)
+                .addPathPatterns("/**") // 인터셉터에 적용할 부분
+                .excludePathPatterns("/", "/members/add", "/login", // 인터셉터에 적용하지 않을 부분
+                        "/logout", "/css/**", "/*.ico","/error");
+    }
+
     // 서블릿 컨테이너 올릴때 필터도 같이 등록된다
     // 실무에서 HTTP 요청시 같은 요청의 로그에 모두 같은 식별자를 자동으로 남기는 방법 : logback mdc
-    @Bean
+    // @Bean
     public FilterRegistrationBean logFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter());
@@ -23,7 +41,7 @@ public class WebConfig {
         return filterRegistrationBean;
     }
 
-    @Bean
+    // @Bean
     public FilterRegistrationBean loginCheckFilter() {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LoginCheckFilter());
